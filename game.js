00fb441cf4ed2142d04567bb54e9ba8f1f7c4220
Noreh AD <akinoreh@gmail.com>
2022-10-13 14:48:04 +0300

    paddle & control

--- game.js
+++ game.js
@@ -8,7 +8,37 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
-var ballRadius = 10;
+const ballRadius = 10;
+
+const paddleHeight = 10;
+const paddleWidth = 70;
+const paddleSpeed = paddleWidth * 0.1;
+const paddleSensitivity = 0.5;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
 
 function drawBall() {
     ctx.beginPath();
@@ -18,14 +48,35 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "hsl(200, 100%, 45%)";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
+    }
+    if (rightPressed) {
+        paddleX += paddleSpeed * paddleSensitivity;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if (leftPressed) {
+        paddleX -= paddleSpeed * paddleSensitivity;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
     }
     x += dx;
     y += dy;
